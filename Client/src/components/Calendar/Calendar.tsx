5c2ec20d07088c93c99997a6b977c7f0a263ab82
Onton <bashprog@gmail.com>
2023-02-13 16:29:52 +0600

    Add calendar's grid

--- Client/src/components/Calendar/Calendar.tsx
+++ Client/src/components/Calendar/Calendar.tsx
@@ -4,9 +4,71 @@ import './Calendar.scss';
 import CalendarControl from "../CalendarControl/CalendarControl";
 
 const Calendar: React.FC = () => {
-    return(
+    return (
         <section className={'calendar'}>
             <CalendarControl/>
+            <div className={'calendar-box'}>
+                <div className={'calendar-days'}>
+                    <span>
+                        Monday
+                    </span>
+                    <span>
+                        Tuesday
+                    </span>
+                    <span>
+                        Wednesday
+                    </span>
+                    <span>
+                        Thursday
+                    </span>
+                    <span>
+                        Friday
+                    </span>
+                    <span>
+                        Saturday
+                    </span>
+                    <span>
+                        Sunday
+                    </span>
+                </div>
+                <div className="calendar-nums">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </section>
     )
 };
